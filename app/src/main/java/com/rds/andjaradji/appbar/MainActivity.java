232628a83ac3b3a57d364de2466b09d895ab18b4
Andjaradji <andjaradji.rooseno@gmail.com>
2019-02-03 09:40:50 +0700

    FIRST Commit for Github

---
+++ app/src/main/java/com/rds/andjaradji/appbar/MainActivity.java
@@ -0,0 +1,34 @@
+package com.rds.andjaradji.appbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar)findViewById(R.id.toolbarID);
+        if (mToolbar != null){
+            setSupportActionBar(mToolbar);
+        }
+
+        mToolbar.setTitle("A Toolbar I Just Created");
+        mToolbar.setSubtitle("With A Subtitle");
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return true;
+    }
+}
